be3d41236ad0b36b8a438495c0400472ab1d7f44
obrookes <dl18206@bristol.ac.uk>
2022-04-22 12:34:03 +0100

    basic dataset initialisation

--- panaf_dataset.py
+++ panaf_dataset.py
@@ -1,5 +1,8 @@
+import json
+import mmcv
 import pickle
 from glob import glob
+from tqdm import tqdm
 from torch.utils.data import Dataset
 
 
@@ -7,22 +10,110 @@ class PanAfDataset(Dataset):
     def __init__(self, data_dir, ann_dir, sequence_len):
         super(PanAfDataset, self).__init__()
 
-        self.videos = glob.glob(f"{data_dir}/**/*.mp4", recursive=True)
-        self.anns = glob.glob(f"{ann_dir}/**/*.pkl", recursive=True)
-        assert(len(self.videos) == len(self.anns))
+        self.data_path = data_dir
+        self.ann_path = ann_dir
+        self.data = glob(f"{data_dir}/**/*.mp4", recursive=True)
+        self.anns = glob(f"{ann_dir}/**/*.json", recursive=True)
+        # assert len(self.data) == len(self.anns), f"{len(self.data)}, {len(self.anns)}"
 
         self.sequence_len = sequence_len
-    
-    def count_apes(self, data):
+        self.samples = []
+
+        self.initialise_dataset()
+
+    def get_videoname(self, path):
+        return path.split("/")[-1].split(".")[0]
+
+    def count_apes(self, ann):
+        # TODO: check all videos index from ape 0, 1, 2...
         ids = []
-        for frame in data['annotations']:
-            for detection in frame['detections']:
-                ids.append(detection['ape_id'])
-        return(max(ids))
+        for frame in ann["annotations"]:
+            for detection in frame["detections"]:
+                ids.append(detection["ape_id"])
+
+        if not ids:
+            return False
+
+        return max(ids)
+
+    def check_ape_exists(self, ann, frame_no, current_ape):
+        ape = False
+
+        for a in ann["annotations"]:
+            if a["frame_id"] == frame_no:
+                for d in a["detections"]:
+                    if d["ape_id"] == current_ape:
+                        ape = True
+        return ape
+
+    def print_samples(self):
+        print(self.samples)
+
+    def __len__(self):
+        return len(self.samples)
 
     def initialise_dataset(self):
-        for video in self.videos:
-            with open(video, 'rb') as handle:
-                data = pickle.load(handle)
-            
-            no_of_apes = self.count_apes(data)
+        for data in tqdm(self.data[:10], desc="Initialising samples", leave=False):
+
+            name = self.get_videoname(data)
+            video = mmcv.VideoReader(data)
+            with open(f"{self.ann_path}/{name}.json", "rb") as handle:
+                ann = json.load(handle)
+
+            # Check no of frames match
+            assert len(video) == len(ann["annotations"])
+
+            no_of_apes = self.count_apes(ann)
+            # TODO: check all apes index from 0...
+            for current_ape in range(0, no_of_apes + 1):
+                frame_no = 1
+
+                while frame_no <= len(video):
+                    if (len(video) - frame_no) < self.sequence_len - 1:
+                        break
+
+                    ape = self.check_ape_exists(ann, frame_no, current_ape)
+
+                    if not ape:
+                        frame_no += 1
+                        continue
+                    else:
+                        insufficient_apes = False
+
+                    for look_ahead_frame_no in range(
+                        frame_no, frame_no + self.sequence_len
+                    ):
+                        ape = self.check_ape_exists(
+                            ann, look_ahead_frame_no, current_ape
+                        )
+
+                        if not ape:
+                            insufficient_apes = True
+                            break
+
+                    if insufficient_apes:
+                        frame_no += self.sequence_len
+                        continue
+
+                    if (len(video) - frame_no) >= self.sequence_len:
+                        self.samples.append(
+                            {
+                                "video": name,
+                                "ape_id": current_ape,
+                                "start_frame": frame_no,
+                            }
+                        )
+                    frame_no += self.sequence_len
+        return
+
+
+def main():
+    dataset = PanAfDataset(
+        data_dir="/home/dl18206/Desktop/phd/data/panaf/acp/videos",
+        ann_dir="/home/dl18206/Desktop/phd/data/panaf/acp/annotations/machine/json/all/long",
+        sequence_len=5,
+    )
+
+
+if __name__ == "__main__":
+    main()

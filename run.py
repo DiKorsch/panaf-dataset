ff3a35e1448b4af1503862c8224df1a7d5607a34
obrookes <dl18206@bristol.ac.uk>
2022-04-22 16:22:48 +0100

    basic function working

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision.utils import make_grid
 from torchvision import transforms
+from dataset.base import PanAfDataset
 
 
 def main():
@@ -12,18 +13,17 @@ def main():
     dataset = PanAfDataset(
         data_dir="/home/dl18206/Desktop/phd/data/panaf/acp/videos",
         ann_dir="/home/dl18206/Desktop/phd/data/panaf/acp/annotations/machine/json/all/long",
-        sequence_len=32,
+        sequence_len=8,
         transform=transform,
     )
 
     print(dataset.__len__())
 
     dataloader = DataLoader(dataset)
-    inputs, sample = next(iter(dataloader))
+    inputs = next(iter(dataloader))
+
     print(inputs.shape)
     print(inputs.squeeze(dim=0).shape)
-
-    print(sample)
 
     fig = plt.figure(figsize=plt.figaspect(0.75))
     inputs = inputs.squeeze(dim=0)

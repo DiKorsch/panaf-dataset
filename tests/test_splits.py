8ddd70901dc0b8f0240a21ae659ca3c09b9a10ef
obrookes <dl18206@bristol.ac.uk>
2022-04-27 16:17:44 +0100

    test split instantiation

---
+++ tests/test_splits.py
@@ -0,0 +1,82 @@
+"""
+Summary: test instantiation of splits.
+"""
+
+from torchvision import transforms
+from dataset import SupervisedPanAf
+
+
+def test_train_split():
+    """Test 5-frame sequence with 5-frame behaviour thresh."""
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Resize((244, 244))]
+    )
+
+    data_dir = "/home/dl18206/Desktop/phd/data/panaf/obfuscated/restructure/data/train"
+    ann_dir = (
+        "/home/dl18206/Desktop/phd/data/panaf/obfuscated/restructure/annotations/train"
+    )
+
+    dataset = SupervisedPanAf(
+        data_dir=data_dir,
+        ann_dir=ann_dir,
+        sequence_len=5,
+        sample_itvl=1,
+        transform=transform,
+        behaviour_threshold=72,
+    )
+
+    assert dataset.count_videos() == 400
+    print(f"Train samples: {dataset.__len__()}")
+    print(f"IujvFoEGd6 samples: {dataset.print_samples_by_video('IujvFoEGd6')}")
+
+
+def test_val_split():
+    """Test 5-frame sequence with 5-frame behaviour thresh."""
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Resize((244, 244))]
+    )
+
+    data_dir = (
+        "/home/dl18206/Desktop/phd/data/panaf/obfuscated/restructure/data/validation"
+    )
+    ann_dir = "/home/dl18206/Desktop/phd/data/panaf/obfuscated/restructure/annotations/validation"
+
+    dataset = SupervisedPanAf(
+        data_dir=data_dir,
+        ann_dir=ann_dir,
+        sequence_len=5,
+        sample_itvl=1,
+        transform=transform,
+        behaviour_threshold=72,
+    )
+
+    assert dataset.count_videos() == 25
+    print(f"Validation samples: {dataset.__len__()}")
+
+
+def test_test_split():
+    """Test 5-frame sequence with 5-frame behaviour thresh."""
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Resize((244, 244))]
+    )
+
+    data_dir = "/home/dl18206/Desktop/phd/data/panaf/obfuscated/restructure/data/test"
+    ann_dir = (
+        "/home/dl18206/Desktop/phd/data/panaf/obfuscated/restructure/annotations/test"
+    )
+
+    dataset = SupervisedPanAf(
+        data_dir=data_dir,
+        ann_dir=ann_dir,
+        sequence_len=5,
+        sample_itvl=1,
+        transform=transform,
+        behaviour_threshold=72,
+    )
+
+    assert dataset.count_videos() == 75
+    print(f"Test samples: {dataset.__len__()}")

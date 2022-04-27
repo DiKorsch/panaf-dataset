332bb248b385c62b26a01a7886c444f1be3d7bdf
obrookes <dl18206@bristol.ac.uk>
2022-04-27 11:55:33 +0100

    remove behaviour threshold tests

--- tests/test_base_multiple.py
+++ tests/test_base_multiple.py
@@ -43,21 +43,3 @@ class TestMultipleApes:
         filename = "1oZGHFgDpe"
         ann = dataset.load_annotation(filename)
         assert dataset.count_apes(ann) == 1
-
-    def test_get_ape_behaviour(self):
-        """Test dataset.verify_ape_ids() method."""
-        dataset = PanAfDataset(
-            data_dir=self.data_dir,
-            ann_dir=self.ann_dir,
-            sequence_len=5,
-            sample_itvl=1,
-            transform=self.transform,
-        )
-
-        # Test annotation with 1 ape
-        filename = "1oZGHFgDpe"
-        ann = dataset.load_annotation(filename)
-        behaviour_ape_0 = dataset.get_ape_behaviour(ann=ann, current_ape=0, frame_no=10)
-        behaviour_ape_1 = dataset.get_ape_behaviour(ann=ann, current_ape=1, frame_no=10)
-        assert behaviour_ape_0 == "walking"
-        assert behaviour_ape_1 == "standing"

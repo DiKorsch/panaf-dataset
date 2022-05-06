557bc91fa5e060e5a6884b3aa2b3656a4f3cc8d9
obrookes <dl18206@bristol.ac.uk>
2022-05-06 15:12:22 +0100

    test sample building

--- tests/test_base_dense.py
+++ tests/test_base_dense.py
@@ -10,58 +10,13 @@ from dataset import PanAfDataset
 
 class TestDense:
 
-    transform = transforms.Compose([transforms.Resize((244, 244))])
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Resize((244, 244))]
+    )
 
     data_dir = "tests/data/single/videos"
     ann_dir = "tests/data/single/annotations"
     dense_dir = "tests/data/single/annotations"
-
-    def test_small_sequence(self):
-        """Test 5-frame sequence."""
-        dataset = PanAfDataset(
-            data_dir=self.data_dir,
-            ann_dir=self.ann_dir,
-            sequence_len=5,
-            sample_itvl=1,
-            stride=5,
-            transform=self.transform,
-        )
-        with open(
-            "tests/data/cases/dense/example_one/annotations/0hu96Jv2As.pkl", "rb"
-        ) as handle:
-            ann = pickle.load(handle)
-
-        dense_sample = dataset.build_dense_sample(ann, "0hu96Jv2As", 0, 1)
-        fig = plt.figure(figsize=(8, 6))
-        grid = make_grid(dense_sample).permute(1, 2, 0)
-        plt.imshow(grid)
-        plt.show()
-
-        assert len(dense_sample) == 5
-
-    def test_dense_sequence(self):
-        """Test 5-frame sequence."""
-        dataset = PanAfDataset(
-            data_dir=self.data_dir,
-            ann_dir=self.ann_dir,
-            sequence_len=8,
-            sample_itvl=10,
-            stride=5,
-            transform=self.transform,
-        )
-        with open(
-            "tests/data/cases/dense/example_two/annotations/0YvgQsXboK.pkl", "rb"
-        ) as handle:
-            ann = pickle.load(handle)
-
-        dense_sample = dataset.build_dense_sample(ann, "0YvgQsXboK", 0, 1)
-        fig = plt.figure(figsize=(8, 6))
-        dense_sample = list(reversed(dense_sample))
-        grid = make_grid(dense_sample).permute(1, 2, 0)
-        plt.imshow(grid)
-        plt.show()
-
-        assert len(dense_sample) == 8
 
     def test_check_dense_exists_true(self):
         """Test 5-frame sequence."""
@@ -115,6 +70,22 @@ class TestDense:
             transform=self.transform,
         )
 
-        ann = dataset.get_dense_annotation('0YvgQsXboK')
+        ann = dataset.get_dense_annotation("0YvgQsXboK")
 
-        assert ann['video'] == '0YvgQsXboK'
+        assert ann["video"] == "0YvgQsXboK"
+
+    def test_get_dense_sample(self):
+
+        dataset = PanAfDataset(
+            data_dir=self.data_dir,
+            ann_dir=self.ann_dir,
+            dense_dir=self.dense_dir,
+            sequence_len=5,
+            sample_itvl=1,
+            stride=5,
+            transform=self.transform,
+        )
+
+        dataloader = DataLoader(dataset)
+        sample = next(iter(dataloader))
+        assert len(sample['spatial_sample'].squeeze(dim=0)) == 5

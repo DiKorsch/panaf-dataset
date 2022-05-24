82c9649874eccd72d56197f960d79a0025af76ea
obrookes <dl18206@bristol.ac.uk>
2022-05-24 10:24:50 +0100

    add flow to initialisation

--- tests/dataset/visual/test_visual.py
+++ tests/dataset/visual/test_visual.py
@@ -1,8 +1,10 @@
 import pickle
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 from torchvision.utils import make_grid
 from torchvision import transforms
 from panaf.datasets import PanAfDataset
+from panaf.datasets import SupervisedPanAf
 
 
 class TestDense:
@@ -12,6 +14,7 @@ class TestDense:
     data_dir = "tests/data/single/videos"
     ann_dir = "tests/data/single/annotations"
     dense_dir = "tests/data/single/annotations"
+    flow_dir = "tests/data/single/flow"
 
     def test_small_sequence(self):
         """Test 5-frame sequence."""
@@ -90,15 +93,3 @@ class TestDense:
             stride=50,
             transform=self.transform,
         )
-        with open(
-            "tests/data/cases/dense/example_two/annotations/0YvgQsXboK_dense.pkl", "rb"
-        ) as handle:
-            ann = pickle.load(handle)
-
-        dense_sample = dataset.build_dense_sample(ann, "0YvgQsXboK", 0, 1)
-        fig = plt.figure(figsize=(8, 6))
-        dense_sample = list(reversed(dense_sample))
-        grid = make_grid(dense_sample).permute(1, 2, 0)
-        print(grid.shape)
-        plt.imshow(grid)
-        plt.show()

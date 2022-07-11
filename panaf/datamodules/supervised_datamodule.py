163da88f0d7603702304974a45375140260ef51c
obrookes <dl18206@bristol.ac.uk>
2022-07-11 12:31:20 +0100

    set which_classes arg to None if not in cfg

--- panaf/datamodules/supervised_datamodule.py
+++ panaf/datamodules/supervised_datamodule.py
@@ -7,6 +7,7 @@ from panaf.samplers import BalancedBatchSampler
 from torchvision import transforms
 from catalyst.data import BalanceClassSampler
 from catalyst.data import DistributedSamplerWrapper
+from configparser import NoOptionError
 
 """
 Trainer args (accelerator, devices, num_nodes, etc…)
@@ -62,7 +63,10 @@ class SupervisedPanAfDataModule(LightningDataModule):
         self.pin_memory = cfg.getboolean("loader", "pin_memory")
         self.sampler = cfg.get("loader", "sampler")
 
-        self.which_classes = cfg.get("dataset", "classes")
+        try:
+            self.which_classes = cfg.get("dataset", "classes")
+        except NoOptionError:
+            self.which_classes = None
 
         super().__init__()
 
@@ -85,7 +89,7 @@ class SupervisedPanAfDataModule(LightningDataModule):
                 type=self.type,
                 transform=self.transform,
                 behaviour_threshold=self.train_threshold,
-                which_classes=self.which_classes
+                which_classes=self.which_classes,
             )
 
             self.validation_dataset = SupervisedPanAf(
@@ -99,7 +103,7 @@ class SupervisedPanAfDataModule(LightningDataModule):
                 type=self.type,
                 transform=self.transform,
                 behaviour_threshold=self.test_threshold,
-                which_classes=self.which_classes
+                which_classes=self.which_classes,
             )
 
         if stage == "test" or stage is None:
